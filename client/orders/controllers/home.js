d9d63f1dba441674713a7b38bde1fb0e1a48cdcc
harzkr <harshaofgenx@gmail.com>
2015-08-14 09:57:57 +0530

    Orders-List-Redesigned

--- client/orders/controllers/home.js
+++ client/orders/controllers/home.js
@@ -1,5 +1,5 @@
-angular.module("zwyft").controller("HomeCtrl", ['$scope', '$rootScope','$meteor', '$mdSidenav', '$location',
-function($scope, $rootScope, $meteor, $mdSidenav, $location){
+angular.module("zwyft").controller("HomeCtrl", ['$scope', '$rootScope','$meteor', '$mdSidenav','$mdDialog', '$location',
+function($scope, $rootScope, $meteor, $mdSidenav,$mdDialog, $location){
 
     $scope.orders = $meteor.collection(Orders).subscribe('orders');
 
@@ -32,5 +32,21 @@ function($scope, $rootScope, $meteor, $mdSidenav, $location){
         $scope.myvalue = false;
         newOrder.date = e;
     };
+    $scope.showConfirm = function(ev) {
+  // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+            .title('Thanks for booking with us')
+            .content('You can see the status of your order or book another one!')
+            .ariaLabel('Lucky day')
+            .ok('Go to My Orders')
+            .cancel('Stay on page')
+            .targetEvent(ev);
+
+        $mdDialog.show(confirm).then(function() {
+            $location.path('/list')
+  }, function() {
+        $location.path('/home');
+  });
+};
     $scope.map = { center: { latitude: 23, longitude: 77 }, zoom: 8 };
 }]);
